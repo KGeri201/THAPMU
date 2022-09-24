b044927c40a9627cc503176210bc91b1ffd98993
KGeri201 <36195747+KGeri201@users.noreply.github.com>
2022-09-24 19:17:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,83 +1,127 @@
 #!/bin/bash
 
-until [ ${#db_name} -gt 0 ]
-do 
-  read -p "Name of your database: " db_name
-done
-until [ ${#db_user} -gt 0 ]
-do
-  read -p "Name of your user: " db_user
-done
-until [ ${#db_pwd} -gt 0 ] && [ $db_pwd == $db_pwd_ctrl ]
-do
-  until [ ${#db_pwd} -gt 0 ]
-  do
-    read -sp "Password for your user: " db_pwd
+echo "  _____ _  _   _   ___ __  __ _   _ "
+echo " |_   _| || | /_\ | _ \  \/  | | | |"
+echo "   | | | __ |/ _ \|  _/ |\/| | |_| |"
+echo "   |_| |_||_/_/ \_\_| |_|  |_|\___/ "
+
+getSettings() {
+  printf "\n-------------- SETTINGS --------------\n"
+
+  until [ ${#db_name} -gt 0 ]
+  do 
+    read -p "Name of your database: " db_name
   done
-  echo ""
-  read -sp "Repeat the password: " db_pwd_ctrl
-  if [ $db_pwd != $db_pwd_ctrl ]
-  then
-    printf "\nPasswords do not match!"
-    db_pwd=""
-  fi
-done
+  until [ ${#db_user} -gt 0 ]
+  do
+    read -p "Name of your user: " db_user
+  done
+  until [ ${#db_pwd} -gt 0 ] && [ $db_pwd == $db_pwd_ctrl ]
+  do
+    until [ ${#db_pwd} -gt 0 ]
+    do
+      read -sp "Password for your user: " db_pwd
+    done
+    echo ""
+    read -sp "Repeat the password: " db_pwd_ctrl
+    if [ $db_pwd != $db_pwd_ctrl ]
+    then
+      printf "\n\nPasswords do not match!\n\n"
+      db_pwd=""
+    fi
+  done
+}
 
-echo ""
+install() {
+  printf "Installing packages...\n"
+  #sudo apt-get install -y apt-transport-https -qq
+  #sudo apt-get install -y software-properties-common wget -qq
+  apt-get install -y wget -qq
+  wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
+  echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/enterprise/deb stable main" | tee /etc/apt/sources.list.d/grafana.list >/dev/null
 
-sudo printf "\nInstalling packages...\n"
-#sudo apt-get install -y apt-transport-https -qq
-#sudo apt-get install -y software-properties-common wget -qq
-sudo apt-get install -y wget -qq
-sudo wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
-echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/enterprise/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list >/dev/null
+  # Install Influxdb, mosquitto, python3 and grafana
+  apt-get install -y influxdb influxdb-client mosquitto mosquitto-clients python3 python3-pip grafana-enterprise -qq
+} 
 
-# Install Influxdb, mosquitto, wget, python3 and grafana
-sudo apt-get install -y influxdb influxdb-client mosquitto mosquitto-clients wget python3 python3-pip grafana-enterprise -qq
+download() {
+  printf "Downloading python skript...\n"
+  # Download MQTTInfluxDBBridge from github
+  wget -q -O MQTTInfluxDBBridge.py https://raw.githubusercontent.com/KGeri201/THAPMU/main/MQTTInfluxDBBridge.py
+}
 
-printf "Downloading files...\n"
-# Download files from github
-sudo wget -q -O requirements.txt https://raw.githubusercontent.com/KGeri201/THAPMU/main/requirements.txt
-sudo wget -q -O MQTTInfluxDBBridge.py https://raw.githubusercontent.com/KGeri201/THAPMU/main/MQTTInfluxDBBridge.py
-sudo wget -q -O /etc/systemd/system/mqttinfluxdbbridge.service https://raw.githubusercontent.com/KGeri201/THAPMU/main/mqttinfluxdbbridge.service
+configureSkript() {
+  printf "Configuring skript...\n"
+  # Set database, user and password
+  sed -i "s|INFLUXDB_DATABASE = 'db_name'|INFLUXDB_DATABASE = '$db_name'|g" MQTTInfluxDBBridge.py
+  sed -i "s|INFLUXDB_PASSWORD = 'db_pwd'|INFLUXDB_PASSWORD = '$db_pwd'|g" MQTTInfluxDBBridge.py
+  sed -i "s|INFLUXDB_USER = 'db_user'|INFLUXDB_USER = '$db_user'|g" MQTTInfluxDBBridge.py
+}
 
-sudo sed -i "s|/root|$PWD|g" /etc/systemd/system/mqttinfluxdbbridge.service
+installRequirements() {
+  printf "Installing python libraries...\n"
+  wget -q -O requirements.txt https://raw.githubusercontent.com/KGeri201/THAPMU/main/requirements.txt
+  # Install python3 libraries
+  pip3 -q install -r requirements.txt
+}
 
-sudo sed -i "s|INFLUXDB_DATABASE = 'db_name'|INFLUXDB_DATABASE = '$db_name'|g" MQTTInfluxDBBridge.py
-sudo sed -i "s|INFLUXDB_USER = 'db_user'|INFLUXDB_USER = '$db_user'|g" MQTTInfluxDBBridge.py
-sudo sed -i "s|INFLUXDB_PASSWORD = 'db_pwd'|INFLUXDB_PASSWORD = '$db_pwd'|g" MQTTInfluxDBBridge.py
+setUpDatabase() {
+  printf "Setting up the database...\n"
+  # Setup Influxdb
+  ## Enable http endpoint
+  sed -i -r ':a;N;$!ba;s/\[http\]\n([^\n]*)\n  # enabled = true/\[http\]\n\1\n  enabled = true/g' /etc/influxdb/influxdb.conf
+  ## Start influxdb service
+  systemctl enable influxdb 
+  systemctl start influxdb
 
-printf "Installing python libraries...\n"
-# Install python3 libraries
-sudo pip3 -q install -r requirements.txt
+  influx -execute "CREATE DATABASE $db_name"
+  influx -database "$db_name" -execute "CREATE USER $db_user WITH PASSWORD '$db_pwd'"
+  influx -database "$db_name" -execute "GRANT ALL ON $db_name TO $db_user"
+}
 
-printf "Setting up the database...\n"
-# Setup Influxdb
-## Start influxdb service
-#sudo systemctl enable influxdb 
-sudo systemctl start influxdb 
-## Enable http endpoint
-sudo sed '/#enabled = true/s/^#//' -i /etc/influxdb/influxdb.conf
-## Restart influxdb service
+startServices() {
+  printf "Starting services...\n"
+  wget -q -O /etc/systemd/system/mqttinfluxdbbridge.service https://raw.githubusercontent.com/KGeri201/THAPMU/main/mqttinfluxdbbridge.service
+  sed -i "s|/root|$PWD|g" /etc/systemd/system/mqttinfluxdbbridge.service
+  #sudo systemctl enable mosquitto
+  systemctl start mosquitto
+  systemctl restart influxdb
+  systemctl enable mqttinfluxdbbridge
+  systemctl start mqttinfluxdbbridge
+  service grafana-server start
+}
 
-## Create Database to store measurements
-## Set name of the database
+finish() {
+  printf "\n##########################################################################\n"
+  printf "# Visit                                                                  #\n"
+  printf "#   http://localhost:3000                                                #\n"
+  printf "#   username: admin                                                      #\n"
+  printf "#   password: admin                                                      #\n"
+  printf "# to setup Grafana to display measurements saved in the influx database. #\n"
+  printf "##########################################################################\n"
+}
 
-#influx
-#CREATE DATABASE "$db_name"
-#USE "$db_name"
-#CREATE USER "$db_user" WITH PASSWORD "$db_pwd"
-#GRANT ALL ON "$db_name" TO "$db_user"
-#EXIT
-
-sudo influx -execute 'CREATE DATABASE "$db_name"; USE "$db_name"; CREATE USER "$db_user" WITH PASSWORD "$db_pwd"; GRANT ALL ON "$db_name" TO "$db_user"'
-
-printf "Starting services...\n"
-#sudo systemctl enable mosquitto
-sudo systemctl start mosquitto
-sudo systemctl restart influxdb
-sudo systemctl enable mqttinfluxdbbridge
-sudo systemctl start mqttinfluxdbbridge
-sudo service grafana-server start
-
+if [ "$1" = "setup" ] || [ -z "$1" ]
+then
+  getSettings
+fi
+printf "\n--------------- SETUP ---------------\n"
+if [ "$1" = "install" ] || [ -z "$1" ]
+then
+  install
+  installRequirements
+  download
+fi
+if [ "$1" = "setup" ] || [ -z "$1" ]
+then
+  configureSkript
+fi
+if [ "$1" = "start" ] || [ -z "$1" ]
+then
+  startServices
+fi
 printf "Done\n"
+if [ "$1" = "start" ] || [ -z "$1" ]
+then
+  finish
+fi
