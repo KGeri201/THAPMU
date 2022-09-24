8376ba00f026f4b0699823dce561e8dabbbf149e
KGeri201 <36195747+KGeri201@users.noreply.github.com>
2022-09-24 22:13:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -138,7 +138,7 @@ then
 fi
 if [ "$1" = "start" ] || [ -z "$1" ]
 then
-  printf "\n--------------- START ----------------\n"
+  printf "\n--------------- START ---------------\n"
   printf "Starting services...\n"
   startServices
 fi
